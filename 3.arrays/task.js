0f70c41d712245511c693a2e54e09082e4bd05b3
Dzenny <batterflyfly96@gmail.com>
2025-03-10 20:13:12 +0300

    Arrays tasks solved

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+      }
+      
+      return arr1.every((num, index) => num === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (users.length === 0 || (gender !== "мужской" && gender !== "женский")) {
+      return 0;
+    }
+
+    const sortByGender = users.filter(user => user.gender === gender);
+    const res = sortByGender.reduce((acc, user) => {
+      acc += user.age;
+      return acc;
+    }, 0)
+    
+    return res / sortByGender.length;
 }
